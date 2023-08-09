48571a956467e6c2731c20372d3c70032e726c8a
Karen1308 <90295210+Karen1308@users.noreply.github.com>
2023-08-08 23:02:05 -0300

    Delete Grupo6 directory

--- Grupo6/src/Footer.jsx
+++
@@ -1,6 +0,0 @@
-function Footer() {
-    return (
-        <h2> Adios , esto es un Footer </h2>
-    )
-}
-export default Footer
--- Grupo6/src/Producto.jsx
+++
@@ -1,8 +0,0 @@
-function Producto(props){
-    return(
-       <div className="Producto">
-            <h2>Nombre: {props.nombre}</h2>
-       </div>
-    )
-}
-export default Producto
